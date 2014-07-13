30c24a1ff2752d0873d09e71ed64d4dab8935176
gitwillian <willian.siqueiras@gmail.com>
2014-07-13 14:45:53 -0300

    Update RunTest.java

--- src/test/java/acceptanceTest/Test/RunTest.java
+++ src/test/java/acceptanceTest/Test/RunTest.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
     //this code will only look into "features/" folder for features
     features = {"features/"},
     glue = { "acceptanceTest.Steps"},
-    format = {"pretty"},
+    format = {"json:target/cucumber.json"},
     tags = { "@test" }
     )
 public class RunTest{
