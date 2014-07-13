a3a7ff65fb2287ca9fa1cd6a00317178a068f3dc
willian <willian@git.com.br>
2014-07-13 12:35:48 -0300

    test

---
+++ src/test/java/acceptanceTest/Steps/SearchProductStep.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acceptanceTest.Steps;
+
+
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+
+
+public class SearchProductStep{
+    
+   
+
+    @Given("^I am on \"([^\"]*)\"$")
+    public void I_am_on(String url) {
+        assert true;
+    }
+
+    @When("^I search for \"([^\"]*)\"$")
+    public void I_search_for(String item) {
+        assert true;
+    }
+
+    @Then("^I should see the item$")
+    public void I_should_see_the_item() {     
+        assert true;
+    }
+}
---
+++ src/test/java/acceptanceTest/Test/RunTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package acceptanceTest.Test;
+
+
+
+import cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+    //this code will only look into "features/" folder for features
+    features = {"features/"},
+    glue = { "acceptanceTest.Steps"},
+    format = {"pretty", "html:target/report", "json:target/cucumber.json" },
+    tags = { "@test" }
+    )
+public class RunTest{
+    
+//    private SearchProduct product;
+//    
+//    @Before
+//    public void antesDoTeste() {
+//        product = new SearchProduct();
+//    }
+//    
+//    @After
+//    public void depoisDoTeste() {
+////        product.fechar();
+//    }
+//    
+//    @Test
+//    public void test() {
+//        String termoPesquisado = "Cucumber";
+////        product.preencherPesquisa(termoPesquisado);
+//        
+////        assertTrue(resultado.todosOsSitesPossuemOTermo(termoPesquisado));
+//    }
+}
