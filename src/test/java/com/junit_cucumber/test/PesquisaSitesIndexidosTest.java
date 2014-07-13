9683485b558250faf298fba9fe6099518f97d8d1
willian <willian@git.com.br>
2014-07-12 23:13:46 -0300

    nov

--- src/test/java/com/junit_cucumber/test/PesquisaSitesIndexidosTest.java
+++ src/test/java/com/junit_cucumber/test/PesquisaSitesIndexidosTest.java
@@ -6,40 +6,19 @@
 
 package com.junit_cucumber.test;
 
-import com.junit_cucumber.pages.PaginaPesquisa;
-import com.junit_cucumber.pages.PaginaResultadoPesquisa;
+import cucumber.junit.Cucumber;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-public class PesquisaSitesIndexidosTest {
-     
-    private PaginaPesquisa pagina;
-    private PaginaResultadoPesquisa resultado;
- 
-    @Before
-    public void antesDoTeste() {
-        pagina = new PaginaPesquisa();
-    }
-     
-    @After
-    public void depoisDoTeste() {
-        pagina.fechar();
-    }
- 
-    @Test
-    public void PesquisarUmTermoQueEstejaPresenteEmSitesIndexados() {
-        String termoPesquisado = "Cucumber";
-        pagina.preencherPesquisa(termoPesquisado);
-        resultado = pagina.clicarPesquisar();
-//        assertTrue(resultado.todosOsSitesPossuemOTermo(termoPesquisado));
-    }
- 
-    @Test
-    public void PesquisarPorUmTermoQueNãoEstejaPresenteEmNenhumSiteIndexado() {
-        String termoPesquisado = "Cucumber";
-        pagina.preencherPesquisa(termoPesquisado);
-        resultado = pagina.clicarPesquisar();
-//        assertTrue(resultado.todosOsSitesPossuemOTermo(termoPesquisado));
-    }
+@RunWith(Cucumber.class)
+@Cucumber.Options(
+    //this code will only look into "features/" folder for features
+    features = {"features/"},
+    glue = { "acceptanceTest.Steps"},
+    format = {"pretty", "html:target/reporter-html", "json:target/cucumber.json" },
+    tags = { "@test" }
+    )
+public class PesquisaSitesIndexidosTest{
 }
